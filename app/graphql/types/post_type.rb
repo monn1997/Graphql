db8a5335aa99e34c134e635ead873f57af59b3bd
tomomi adachi <tomoni1997@gmai.com>
2023-03-07 15:43:55 +0900

    サーバー立ち上げ

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -2,9 +2,6 @@
 
 module Types
   class PostType < Types::BaseObject
-    field :id, ID, null: false
-    field :title, String
-    field :content, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :id, ID, null: false
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,14 +4,10 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :post, PostType
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def post
+      Post.last
     end
   end
 end
